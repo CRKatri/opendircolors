e43fd6d8e48a8f41a22ce1ee66d72858e7d7dce0
Cameron Katri <me@cameronkatri.com>
2021-04-24 21:33:53 -0400

    Add LICENSE, run clang-format

--- lscolors.c
+++ lscolors.c
@@ -1,3 +1,31 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2021
+ *	Cameron Katri.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CAMERON KATRI AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL CAMERON KATRI OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -84,12 +112,14 @@ tols_colors(char *lscolors)
 		sprintf(ls_out + strlen(ls_out), "%s=", types[i]);
 		if (isupper(lscolors[2 * i]))
 			sprintf(ls_out + strlen(ls_out), "01;");
-		if (tolower(lscolors[2 * i])  == 'x')
+		if (tolower(lscolors[2 * i]) == 'x')
 			sprintf(ls_out + strlen(ls_out), "00");
 		else if (tolower(lscolors[2 * i] != 'x'))
-			sprintf(ls_out + strlen(ls_out), "3%i", (int)(strchr(col, tolower(lscolors[2 * i])) - col));
-		if (tolower(lscolors[2 * i + 1])  != 'x')
-			sprintf(ls_out + strlen(ls_out), ";4%i", (int)(strchr(col, tolower(lscolors[2 * i])) - col));
+			sprintf(ls_out + strlen(ls_out), "3%i",
+			    (int)(strchr(col, tolower(lscolors[2 * i])) - col));
+		if (tolower(lscolors[2 * i + 1]) != 'x')
+			sprintf(ls_out + strlen(ls_out), ";4%i",
+			    (int)(strchr(col, tolower(lscolors[2 * i])) - col));
 		sprintf(ls_out + strlen(ls_out), ":");
 	}
 	printf("%s\n", ls_out);
